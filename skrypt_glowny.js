63ae4945787416d8aa6798e59aed441da423f398
dejwidek77 <filekgx@gmail.com>
2024-11-09 21:23:41 +0100

    update

--- skrypt_glowny.js
+++ skrypt_glowny.js
@@ -1,26 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form"); 
   const wrapper = document.querySelector(".wrapper"); 
-  form.addEventListener("submit", (event) => {
-    event.preventDefault(); // Zapobiega przeładowaniu strony
 
-    
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
     const usernameInput = form.querySelector("input[type='text']").value.trim();
     if (usernameInput) {
       
       wrapper.classList.add("fadeOut");
 
-    
       setTimeout(() => {
-        window.location.href = "headpage.html";
+        window.location.href = "headpage.html"; 
       }, 1400); 
     }
   });
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
   container.classList.add("fadeIn"); 
 });
-
