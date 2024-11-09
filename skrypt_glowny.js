535930215e3bdf4de441e32c26980c07f3666577
dejwidek77 <filekgx@gmail.com>
2024-11-09 17:20:35 +0100

    zzz

--- skrypt_glowny.js
+++ skrypt_glowny.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form"); // Pobiera formularz z HTML
-  const wrapper = document.querySelector(".wrapper"); // Pobiera wrapper, aby dodać animację
-
+  const form = document.querySelector("form"); 
+  const wrapper = document.querySelector(".wrapper"); 
   form.addEventListener("submit", (event) => {
     event.preventDefault(); // Zapobiega przeładowaniu strony
 
-    // Sprawdza, czy pole tekstowe jest wypełnione
+    
     const usernameInput = form.querySelector("input[type='text']").value.trim();
     if (usernameInput) {
-      // Dodaje efekt zanikania do wrappera
+      
       wrapper.classList.add("fadeOut");
 
-      // Po 800ms (czas trwania animacji) przenosi na główną stronę
+    
       setTimeout(() => {
         window.location.href = "headpage.html";
-      }, 800); // Dopasowanie do długości animacji
+      }, 1400); 
     }
   });
 });
@@ -22,6 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
-  container.classList.add("fadeIn"); // Dodaj efekt pojawiania się
+  container.classList.add("fadeIn"); 
 });
 
